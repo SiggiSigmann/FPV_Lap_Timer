32877875dfa363ed8cf3c7dc7e059254b314fd39
SiggiSigmann <siggisigmann@googlemail.com>
2021-09-13 13:46:31 +0200

    add menu to main

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #include "RX5808/SPI_RX5808.h"
 #include "RX5808/channels.h"
 
+#include "menu/MainMenu.h"
+#include "menu/Windows/Osciloskope.h"
+#include "free.h"
+
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
@@ -17,10 +21,14 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 SPI_RX5808* rx;
 
+MainMenu* mm;
+
+
 void setup(){
   Serial.begin(9600);
   while (!Serial){}
   Serial.println("setup");
+  Serial.println(freeMemory());
 
   //display
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) { 
@@ -28,25 +36,25 @@ void setup(){
     for(;;); // Don't proceed, loop forever
   }
   display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
   display.display();
- 
+
+
+  mm = new MainMenu(&display);
+
+  Serial.println(freeMemory());
+  
+  Osciloskope* o = new Osciloskope(&display, mm);
+  mm->setMainScreen(o);
+  Serial.println(freeMemory());
+
   //Rx
   rx = new SPI_RX5808(A0);
+
+  Serial.println("end setup");
 }
 
 void loop() {
-  for(int i = 1; i<channelAmount*3;i+=3){
-
-    //get frequenzy out of array, sorted by value
-    rx->setFreq(pgm_read_word_near(channelFreqTable+pgm_read_word_near(channelList+i/3)));
-    rx->waitTillValid();
-
-    //get rssi level
-    short level = rx->getRSSI()/10;
-
-    //display level
-    display.drawFastVLine(i+1+4,0,SCREEN_HEIGHT,BLACK);
-    display.drawPixel(i+1+4,60-level,WHITE);
-    display.display();
-  }
+  mm->run();
 }
